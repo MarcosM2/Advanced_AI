cad9f20710e1a13b4bb71e03370123a892b58473
MarcosM2 <mmaher1@tcd.ie>
2026-03-31 21:22:01 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -2,5 +2,4 @@ python -m venv venv
 call venv\Scripts\activate
 python -m pip install --upgrade pip
 pip install -r requirements.txt
-hf download bartowski/Qwen2.5-0.5B-Instruct-GGUF \
---include "Qwen2.5-0.5B-Instruct-Q4_K_M.gguf" --local-dir ./models
+hf download bartowski/Qwen2.5-0.5B-Instruct-GGUF --include "Qwen2.5-0.5B-Instruct-Q4_K_M.gguf" --local-dir ./models
