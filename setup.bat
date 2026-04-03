7fa647ee273fba09fd93af21fb131510dc1793a1
MarcosM2 <mmaher1@tcd.ie>
2026-04-03 20:40:17 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -2,4 +2,5 @@ python -m venv venv
 call venv\Scripts\activate
 python -m pip install --upgrade pip
 pip install -r requirements.txt
+set HF_HUB_DISABLE_XET=1
 hf download bartowski/Qwen2.5-0.5B-Instruct-GGUF --include "Qwen2.5-0.5B-Instruct-Q4_K_M.gguf" --local-dir ./models
