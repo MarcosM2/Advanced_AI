5ff50c4504a865bdc882d1ab2c778d03db31bd27
MarcosM2 <mmaher1@tcd.ie>
2026-04-03 14:48:16 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -2,4 +2,5 @@ python -m venv venv
 call venv\Scripts\activate
 python -m pip install --upgrade pip
 pip install -r requirements.txt
+pip install llama-cpp-python huggingface_hub
 hf download bartowski/Qwen2.5-0.5B-Instruct-GGUF --include "Qwen2.5-0.5B-Instruct-Q4_K_M.gguf" --local-dir ./models
